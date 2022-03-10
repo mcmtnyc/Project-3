b53c3c00fe194c3355abdec8effd48633f219be4
tmelnyc <tmelnyc@gmail.com>
2022-03-10 15:50:16 -0500

    Woring on formatting modal

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 /// Make bar bg
 /// H1 "You miss every shot you don't take... maybe today is the day..."
 /// H2 "click for pickup line to try"
-let hatButton = document.querySelector(".hatButton")
-hatButton.addEventListener("click", openModal)
 
 /// In modal
 /// Default modal
@@ -24,67 +22,26 @@ hatButton.addEventListener("click", openModal)
 /// Hidden press ESC to close Modal
 
 // Get the modal
-var modal = document.getElementById("myModal");
-
+let modal = document.querySelector(".modal");
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let hatButton = document.querySelector(".hatButton")
+hatButton.addEventListener("click", () => { modal.style.display = "block" })
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// How to close modal
+let exitButtons = document.querySelectorAll(".close")
+exitButtons.addEventListener("click", () => { modal.style.display = "none" })
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
-}
+// let unimpressedWaifu = document.q
+// /// fetch random waifu from sfw/cringe and make into call-abled image
+// let waifuStage = document.querySelector(".modalRight")
+// /// Define where waifu image appears, RIGHT SIDE OF MODAL
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-
-
-
-let unimpressedWaifu = document.q
-/// fetch random waifu from sfw/cringe and make into call-abled image
-let waifuStage = document.querySelector(".modalRight")
-/// Define where waifu image appears, RIGHT SIDE OF MODAL
-
-let pickUpLine =
-  fetch('https://getpickuplines.herokuapp.com/lines/random').then((response) => {
-    return response.json()
-  }).then((result) => {
-    console.log(result)
-  })
-/// fetch pickup line
-let speechBubble = document.querySelector(".speechBubble")
-/// Define where fetched line will go, HEADER BLOCK OF MODAL
-
-
-
-function openModal() {
-
-  /// Make modal active
-  /// Populate modal with pickup line at top
-  let speechBubble = document.querySelector(".speechBubble")
-  let pickUpLine = speechBubble.textContent()
-
-  unimpressedWaifu = waifuStage.innerHTML("img src = "WHAT IS CALLED"")
-
-
-  let modalCurtainStage = document.querySelector(".modalLeft")
-  modalCurtain.addEventListener("mouseover", liftCurtain)
-
-  function liftCurtain() {
-
-  }
-
+// let pickUpLine =
+//   fetch('https://getpickuplines.herokuapp.com/lines/random').then((response) => {
+//     return response.json()
+//   }).then((result) => {
+//     console.log(result)
+//   })
+// /// fetch pickup line
+// let speechBubble = document.querySelector(".speechBubble")
+// /// Define where fetched line will go, HEADER BLOCK OF MODAL
